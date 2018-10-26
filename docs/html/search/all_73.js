a0dc8a94ff37195475b40e0256dad1082050088e
alex <alex@staticlibs.net>
2018-10-27 00:12:21 +0100

    1.0.4 docs

--- docs/html/search/all_73.js
+++ docs/html/search/all_73.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['mustache',['mustache',['../namespacestaticlib_1_1mustache.html',1,'staticlib']]],
-  ['source',['source',['../classstaticlib_1_1mustache_1_1source.html#aa59d376c692bf6d5b0e3020e9ee03342',1,'staticlib::mustache::source']]],
+  ['source',['source',['../classstaticlib_1_1mustache_1_1source.html#aa59d376c692bf6d5b0e3020e9ee03342',1,'staticlib::mustache::source::source(const std::string &amp;mustache_file_path, const sl::json::value &amp;json, const std::map&lt; std::string, std::string &gt; &amp;partials=std::map&lt; std::string, std::string &gt;())'],['../classstaticlib_1_1mustache_1_1source.html#ae787fa1b077f05e13910f8def6ce03bc',1,'staticlib::mustache::source::source(const sl::json::value &amp;json, const std::string &amp;mustache_file_contents, const std::map&lt; std::string, std::string &gt; &amp;partials=std::map&lt; std::string, std::string &gt;())']]],
   ['source',['source',['../classstaticlib_1_1mustache_1_1source.html',1,'staticlib::mustache']]],
   ['source_2ehpp',['source.hpp',['../source_8hpp.html',1,'']]],
   ['staticlib',['staticlib',['../namespacestaticlib.html',1,'']]]
--- docs/html/search/functions_73.js
+++ docs/html/search/functions_73.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['source',['source',['../classstaticlib_1_1mustache_1_1source.html#aa59d376c692bf6d5b0e3020e9ee03342',1,'staticlib::mustache::source']]]
+  ['source',['source',['../classstaticlib_1_1mustache_1_1source.html#aa59d376c692bf6d5b0e3020e9ee03342',1,'staticlib::mustache::source::source(const std::string &amp;mustache_file_path, const sl::json::value &amp;json, const std::map&lt; std::string, std::string &gt; &amp;partials=std::map&lt; std::string, std::string &gt;())'],['../classstaticlib_1_1mustache_1_1source.html#ae787fa1b077f05e13910f8def6ce03bc',1,'staticlib::mustache::source::source(const sl::json::value &amp;json, const std::string &amp;mustache_file_contents, const std::map&lt; std::string, std::string &gt; &amp;partials=std::map&lt; std::string, std::string &gt;())']]]
 ];
